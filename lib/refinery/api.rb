a8b0c3aa67ef4d44c92a3fc77a73e1a11f207d03
Brice Sanchez <brice@bisscomm.com>
2016-04-06 14:11:25 -0400

    Fix engine

--- lib/refinery/api.rb
+++ lib/refinery/api.rb
@@ -1,10 +1,26 @@
-require 'refinery/core'
-
+require 'refinerycms-core'
+require 'responders'
 require 'rabl'
 
 module Refinery
+  autoload :ApiGenerator, 'generators/refinery/api/api_generator'
+
   module Api
+    require 'refinery/api/engine'
+    require 'refinery/api/configuration'
+
+    autoload :Version, 'refinery/api/version'
+
+    class << self
+      attr_writer :root
+
+      def root
+        @root ||= Pathname.new(File.expand_path('../../../', __FILE__))
+      end
+
+      def factory_paths
+        @factory_paths ||= [ root.join("spec/factories").to_s ]
+      end
+    end
   end
 end
-
-require 'refinery/api/engine'
--- lib/refinery/api/engine.rb
+++ lib/refinery/api/engine.rb
@@ -1,39 +1,48 @@
-require 'rails/engine'
 require 'versioncake'
 
 module Refinery
   module Api
     class Engine < Rails::Engine
-      isolate_namespace Refinery
-      engine_name 'refinery_api'
+      include Refinery::Engine
 
-      Rabl.configure do |config|
-        config.include_json_root = false
-        config.include_child_root = false
+      isolate_namespace Refinery::Api
+      engine_name :refinery_api
 
-        # Motivation here it make it call as_json when rendering timestamps
-        # and therefore display miliseconds. Otherwise it would fall to
-        # JSON.dump which doesn't display the miliseconds
-        config.json_engine = ActiveSupport::JSON
-      end
+      config.autoload_paths += %W( #{config.root}/lib )
 
-      config.versioncake.supported_version_numbers = [1]
-      config.versioncake.extraction_strategy = :http_header
+      # config.before_configuration do
+        Rabl.configure do |config|
+          config.include_json_root = false
+          config.include_child_root = false
 
-      initializer "refinery.api.environment", :before => :load_config_initializers do |app|
-        Refinery::Api::Config = Refinery::ApiConfiguration.new
-      end
-
-      def self.activate
-        Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
-          Rails.configuration.cache_classes ? require(c) : load(c)
+          # Motivation here it make it call as_json when rendering timestamps
+          # and therefore display miliseconds. Otherwise it would fall to
+          # JSON.dump which doesn't display the miliseconds
+          config.json_engine = ActiveSupport::JSON
         end
-      end
-      config.to_prepare &method(:activate).to_proc
 
-      def self.root
-        @root ||= Pathname.new(File.expand_path('../../../../', __FILE__))
+        config.versioncake.supported_version_numbers = [1]
+        config.versioncake.extraction_strategy = :http_header
+      # end
+
+      config.after_initialize do
+        Refinery.register_engine(Refinery::Api)
       end
+
+      # initializer "refinery.api.environment", :before => :load_config_initializers do |app|
+      #   # Refinery::Api::Config = Refinery::ApiConfiguration.new
+      # end
+
+      # def self.activate
+      #   Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
+      #     Rails.configuration.cache_classes ? require(c) : load(c)
+      #   end
+      # end
+      # config.to_prepare &method(:activate).to_proc
+
+      # def self.root
+      #   @root ||= Pathname.new(File.expand_path('../../../../', __FILE__))
+      # end
     end
   end
 end
