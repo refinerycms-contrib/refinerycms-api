bcd160e087a554be62c020c11d220f915839cb88
Brice Sanchez <contact@brice-sanchez.com>
2016-05-15 01:08:24 -0400

    WiP Fix version spec

--- spec/requests/version_spec.rb
+++ spec/requests/version_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe "Version", type: :request do
-  let!(:countries) { 2.times.map { create :country } }
+  let!(:pages) { 2.times.map { create :page } }
 
   describe "/api" do
     it "be a redirect" do
-      get "/api/countries"
+      get "/api/pages"
       expect(response).to have_http_status 301
     end
   end
 
   describe "/api/v1" do
     it "be successful" do
-      get "/api/v1/countries"
+      get "/api/v1/pages"
       expect(response).to have_http_status 200
     end
   end
