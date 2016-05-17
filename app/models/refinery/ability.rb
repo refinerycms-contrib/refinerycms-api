05ccb816d4f67c207831534cd8a7472d0466adfd
Brice Sanchez <contact@brice-sanchez.com>
2016-05-17 00:56:32 -0400

    Add Cancan ability

---
+++ app/models/refinery/ability.rb
@@ -0,0 +1,57 @@
+# Implementation class for Cancan gem.  Instead of overriding this class, consider adding new permissions
+# using the special +register_ability+ method which allows extensions to add their own abilities.
+#
+# See http://github.com/ryanb/cancan for more details on cancan.
+require 'cancan'
+module Refinery
+  class Ability
+    include CanCan::Ability
+
+    class_attribute :abilities
+    self.abilities = Set.new
+
+    # Allows us to go beyond the standard cancan initialize method which makes it difficult for engines to
+    # modify the default +Ability+ of an application.  The +ability+ argument must be a class that includes
+    # the +CanCan::Ability+ module.  The registered ability should behave properly as a stand-alone class
+    # and therefore should be easy to test in isolation.
+    def self.register_ability(ability)
+      self.abilities.add(ability)
+    end
+
+    def self.remove_ability(ability)
+      self.abilities.delete(ability)
+    end
+
+    def initialize(user)
+      self.clear_aliased_actions
+
+      # override cancan default aliasing (we don't want to differentiate between read and index)
+      alias_action :delete, to: :destroy
+      alias_action :edit, to: :update
+      alias_action :new, to: :create
+      alias_action :new_action, to: :create
+      alias_action :show, to: :read
+      alias_action :index, :read, to: :display
+      alias_action :create, :update, :destroy, to: :modify
+
+      user ||= Refinery::Api.user_class.new
+
+      if user.respond_to?(:has_role?) && user.has_role?('superuser')
+        can :manage, :all
+      else
+        can :display, Image
+        can :display, Page
+        can :display, Resource
+      end
+
+      # Include any abilities registered by extensions, etc.
+      Ability.abilities.each do |clazz|
+        ability = clazz.send(:new, user)
+        @rules = rules + ability.send(:rules)
+      end
+
+      # Protect admin role
+      cannot [:update, :destroy], Role, name: ['superuser'] if defined?(Role)
+    end
+  end
+end
